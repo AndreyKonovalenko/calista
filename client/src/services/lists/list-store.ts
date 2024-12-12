54d16d352068c02bc2e19aba9803d34840c31a7b
Andrey Konovalneko <andreinew@mail.ru>
2024-12-12 15:46:35 +0300

    chore: fix typing

--- client/src/services/lists/list-store.ts
+++ client/src/services/lists/list-store.ts
@@ -1,6 +1,6 @@
-// export type TList = {
-//   _id: string;
-//   title: string;
-//   creater_id: string;
-//   cards: Array<String>;
-// };
+export type TList = {
+  _id: string;
+  title: string;
+  creater_id: string;
+  cards: Array<string>;
+};
