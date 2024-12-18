103b8ae9e80a86c1faf11417622a680c2b7814eb
Andrey Konovalneko <andreinew@mail.ru>
2024-12-18 13:52:30 +0300

    chore: save

--- client/src/hooks/useSse.ts
+++ client/src/hooks/useSse.ts
@@ -1,9 +1,17 @@
-import {useEffect} from 'react'
+import {useEffect, useState} from 'react'
 import api from '../utils/calista-api'
 
 const useSse = () => {
+
+  const [status, setStatus]= useState(4)
+  
+  useEffect(() => {
+    console.log(status) 
+  }, [status])
+
   useEffect(()=> {
     const eventSource = api.sse.setConnection() 
+    setStatus(eventSource.readyState)
     return () => {
       console.log('clean up')
       eventSource.close()
