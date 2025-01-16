7f571fd5dea5377aeb44af99b308ec9b241abc18
Andrey Konovalneko <andreinew@mail.ru>
2025-01-16 16:17:34 +0300

    chore: work on test

--- server/src/__tests__/app.test.ts
+++ server/src/__tests__/app.test.ts
@@ -1,10 +1,15 @@
 import app from '../server';
 import request from 'supertest'
+import { connectDB } from '../db';
+
+beforeEach(async() => {
+  await connectDB()
+});
 
 describe("GET /", () => {
-  it('respond with "Hello World', async ()=> {
+  it('respond with "Hello World', async () => {
     const response = await request(app).get("/");
     expect(response.status).toBe(200);
     expect(response.text).toBe("Hello World");
-  })
+  }, 10000)
 })
