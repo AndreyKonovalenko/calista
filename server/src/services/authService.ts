0f504bfb1ed369596d6526c95af503b0edb9975d
Andrey Konovalneko <andreinew@mail.ru>
2025-01-29 17:20:33 +0300

    chore: save

--- server/src/services/authService.ts
+++ server/src/services/authService.ts
@@ -1,21 +1,23 @@
 import { Types } from 'mongoose';
 import { Response } from 'express';
 import { JWT_SECRET, NODE_ENV, TOKEN_EXPIRES_IN } from '../config';
+import { HydratedDocument } from 'mongoose';
 import jwt from 'jsonwebtoken';
+import { UserModal, IUser } from '../models/UserModel';
 
-// export async function registerUser(
-//   user: HydratedDocument<IUser>,
-// ): Promise<null | HydratedDocument<IUser>> {
-//   const newUser: HydratedDocument<IUser> = await UserModal.create(user);
-//   return newUser ? newUser : null;
-// }
+export async function createUser(
+  user: IUser,
+): Promise<null | HydratedDocument<IUser>> {
+  const newUser: HydratedDocument<IUser> = await UserModal.create(user);
+  return newUser;
+}
 
-// export async function findUserByUsername(
-//   username: string,
-// ): Promise<null | HydratedDocument<IUser>> {
-//   const user = await UserModal.findOne({ username });
-//   return user;
-// }
+export async function findUserByUsername(
+  username: string,
+): Promise<null | HydratedDocument<IUser>> {
+  const user = await UserModal.findOne({ username });
+  return user;
+}
 
 export function generateToken(
   user_id: Types.ObjectId,
