3d8440e302bfb4a7da464675baf1872829680d53
AndreyKonovalenko <andreinew@mail.ru>
2025-02-09 17:56:30 +0300

    chore: save

--- server/src/middleware/validation-handler.ts
+++ server/src/middleware/validation-handler.ts
@@ -2,20 +2,25 @@ import { StatusCodes } from 'http-status-codes';
 import { CustomError } from '../utils/CustomError';
 import { Request, Response, NextFunction } from 'express';
 
-import Joi, {ValidationError} from 'joi';
+import Joi, { ValidationError, ValidationResult } from 'joi';
 
 export const validationHandler = (schema: Joi.Schema) => {
-  return async (req: Request, _res: Response, next: NextFunction) => {   
+  return async (req: Request, _res: Response, next: NextFunction) => {
     try {
-      const validated = await schema.validateAsync(req.body);
+      const validated: ValidationResult = await schema.validateAsync(req.body);
       req.body = validated;
-      next()
+      next();
     } catch (err) {
       if (err instanceof ValidationError) {
-        console.error(err.details[0].type)
-        next(new CustomError(err.details[0].message, StatusCodes.UNPROCESSABLE_ENTITY));
+        console.error(err.details[0].type);
+        next(
+          new CustomError(
+            err.details[0].message,
+            StatusCodes.UNPROCESSABLE_ENTITY,
+          ),
+        );
       }
-      next(err)
+      next(err);
     }
   };
 };
