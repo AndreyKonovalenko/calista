a3c0f10ebe76f9016514f1e2da99726ac334d40d
Andrey Konovalneko <andreinew@mail.ru>
2025-12-12 14:54:29 +0300

    chore: added token expiration check in protected  middleware

--- server/src/middleware/protected.ts
+++ server/src/middleware/protected.ts
@@ -4,18 +4,15 @@ import { StatusCodes, ReasonPhrases } from 'http-status-codes';
 import { CustomError } from '../utils/CustomError';
 import { IUser, UserModel } from '../models/UserModel';
 import { HydratedDocument } from 'mongoose';
+import { asyncHandler } from '../utils/async-handler';
 import config from '../config';
 
 export interface CustomRequest extends Request {
   user: HydratedDocument<IUser>;
 }
 
-export const protect = async (
-  req: Request,
-  _res: Response,
-  next: NextFunction,
-): Promise<void> => {
-  try {
+export const protect = asyncHandler(
+  async (req: Request, _res: Response, next: NextFunction): Promise<void> => {
     const token = req.cookies.jwt;
     if (!token) {
       throw new CustomError(
@@ -24,16 +21,25 @@ export const protect = async (
       );
     }
     const decoded = jwt.verify(token, config.app.jwtSecret) as JwtPayload;
-    // need to set expiration check
+    if (!decoded || !decoded.exp)
+      throw new CustomError('Error decoding token', StatusCodes.UNAUTHORIZED);
+    const currentTime = Date.now() / 1000;
+    if (decoded.exp && decoded.exp < currentTime)
+      throw new CustomError('Token expired', StatusCodes.UNAUTHORIZED);
     const user: HydratedDocument<IUser> | null = await UserModel.findById(
       decoded.user_id,
     ).select('-password');
-    if (user) {
-      (req as CustomRequest).user = user;
-      // need to add isVarified checking
-      next();
-    }
-  } catch (error) {
-    next(error);
-  }
-};
+    if (!user)
+      throw new CustomError(
+        ReasonPhrases.UNAUTHORIZED,
+        StatusCodes.UNAUTHORIZED,
+      );
+    if (user && !user.isVerified)
+      throw new CustomError(
+        'you have not verified your account',
+        StatusCodes.FORBIDDEN,
+      );
+    (req as CustomRequest).user = user;
+    next();
+  },
+);
