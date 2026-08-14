af8a0d46e9ae2fdb8bef59b65ef4d9c377f70d56
Andrey Konovalneko <andreinew@mail.ru>
2026-08-14 16:15:42 +0300

    chore: remve useEffect for card-page-on-background component that couse error

--- client/src/pages/card-page/card-page-on-background.tsx
+++ client/src/pages/card-page/card-page-on-background.tsx
@@ -1,11 +1,9 @@
-import React,{useEffect} from 'react';
+import React from 'react';
 import { useParams } from 'react-router';
 import { useCard } from '../../services/card-store';
 import Card from '../../components/card-page-components/card/card';
-import { debugLog } from '../../utils/debug';
 
 const CardPageOnBackground = () => {
-  debugLog('')
   const { cardId } = useParams();
   if (!cardId) {
     return;
@@ -15,10 +13,6 @@ const CardPageOnBackground = () => {
     return;
   }
   const { _id, boardId, description, name } = card;
-
-  useEffect(()=>{
-    debugLog('card-page-on-backgroung')
-  },[])
   return card ? (
     <Card _id={_id} boardId={boardId} name={name} description={description} />
   ) : null;
